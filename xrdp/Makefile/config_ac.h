9901a73775039e3440d2e166df2d1a9921153c05
Jay Sorg <jay.sorg@gmail.com>
2017-10-26 14:51:14 -0700

    xrdp: add some defines to config_ac.h

--- xrdp/Makefile/config_ac.h
+++ xrdp/Makefile/config_ac.h
@@ -5,7 +5,22 @@
 #if !defined(_GNU_SOURCE)
 #define _GNU_SOURCE 1
 #endif
+
 #define HAVE_STDINT_H 1
+#define HAVE_DLFCN_H 1
+#define HAVE_FUNC_ATTRIBUTE_FORMAT 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_SYS_PRCTL_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE__PAM_TYPES_H 1
+
 #define XRDP_LOG_PATH "/tmp"
 #define PACKAGE_VERSION "0.9.4"
 
