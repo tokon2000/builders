b69dc5bfb1a11ad2b7031f617ade2a08a418e7a5
Jay Sorg <jay.sorg@gmail.com>
2017-10-26 14:20:13 -0700

    xrdp: add base64 common file

--- xrdp/Makefile/config_ac.h
+++ xrdp/Makefile/config_ac.h
@@ -2,9 +2,17 @@
 #if !defined _CONFIG_AC_H
 #define _CONFIG_AC_H
 
-#define _GNU_SOURCE
+#if !defined(_GNU_SOURCE)
+#define _GNU_SOURCE 1
+#endif
 #define HAVE_STDINT_H 1
 #define XRDP_LOG_PATH "/tmp"
-#define PACKAGE_VERSION "0.9.2"
+#define PACKAGE_VERSION "0.9.4"
+
+#define XRDP_SOCKET_PATH "/tmp/.xrdp"
+#define XRDP_CFG_PATH "/etc/xrdp"
+#define XRDP_PID_PATH "/var/run"
+#define XRDP_SHARE_PATH "/usr/share"
+#define XRDP_MODULE_PATH "/usr/local/lib/xrdp"
 
 #endif
